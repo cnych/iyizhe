79e9d84376eb9459a597ad242bbc43bf0c4374e1
chuanhu.yang <ych_1024@163.com>
2013-11-14 17:33:42 +0800

    change project name
    change default project name to iyizhe!

--- iyizhe/settings.py
+++ iyizhe/settings.py
@@ -1,4 +1,4 @@
-# Django settings for mysite project.
+# Django settings for iyizhe project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -101,10 +101,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'iyizhe.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'iyizhe.wsgi.application'
 
 TEMPLATE_DIRS = (
 # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
